967a94e71df50b7861c921b007674884829d351c
Murat <muratuseini@gmail.com>
2023-03-01 17:46:46 +0100

    finished 3rd lab

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
+import './course.dart';
+import './add_new_elem.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -7,68 +9,82 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Termins',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.orange,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Termins'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
+  int auto_increment_value = 2;
+  MyHomePage({super.key, required this.title});
+  String title;
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+   void _addItemFunction(BuildContext ct) {
+    showModalBottomSheet(
+        context: ct,
+        builder: (_) {
+          return GestureDetector(onTap: () {}, child: NewCourseTermin(_addNewTerminToList,widget.auto_increment_value), behavior: HitTestBehavior.opaque);
+        });
+      widget.auto_increment_value +=1;
+  }
+  void _addNewTerminToList(Course new_course_termin) {
     setState(() {
-      _counter++;
+      this.courses.add(new_course_termin);
     });
   }
-
+  List<Course> courses = [
+    Course(1, "Computer Networks", DateTime.parse("2023-03-26 12:30:00")),
+    Course(2, "Structured Programming", DateTime.parse("2023-03-27 14:45:00"))
+  ];
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
+      floatingActionButton: FloatingActionButton(child: Icon(Icons.add),onPressed: () =>print(this.courses.toString()),),
       appBar: AppBar(
-
         title: Text(widget.title),
+        actions: [
+          IconButton(onPressed: () => _addItemFunction(context), icon: Icon(Icons.add),tooltip: "Create new termin",),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView.builder(
+        itemCount: this.courses.length,
+        itemBuilder: (ctx,index){
+          return Card(
+            margin: EdgeInsets.all(10),
+            elevation: 5,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(15),
+                  child: Text(
+                    courses.elementAt(index).course_name,
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold,color: Theme.of(ctx).primaryColorLight),
+                  ),
+                ),
+                Container(
+                  child: Text("Date: ${DateFormat.yMMMMEEEEd().format(courses.elementAt(index).termin_date)} Time: ${DateFormat.jm().format(courses.elementAt(index).termin_date)}")
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
